1168741e78f93f733d0174d3aa801cf2ca875ba9
Winter-Row <winter.e.r@outlook.com>
2023-02-03 12:31:12 -0500

    Made some more style changes to the wireframes and added in an additional wireframe activity.

--- app/src/main/java/com/lh1169835/movietracker/AddMovieActivity.kt
+++ app/src/main/java/com/lh1169835/movietracker/AddMovieActivity.kt
@@ -3,7 +3,7 @@ package com.lh1169835.movietracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddMovie : AppCompatActivity() {
+class AddMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_movie)
